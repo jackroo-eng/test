fb5f40220348560d75206345025fee237b644647
jackroo-eng <63950589+jackroo-eng@users.noreply.github.com>
2020-04-23 09:40:26 +0600

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,36 @@
+#python 2.7.15
+print("lets start the quiz")
+print("which is smallest country in world")
+x=int(input(" 1 for India \n 2 for russia \n 3 for china"))
+if x==1 :
+ print("you are foolish")
+ print("____________________________________________________________")
+elif x==2 :
+  print("you are fool")
+  print("____________________________________________________________")
+else :
+ print("you are mad")
+ print("_____________________________________________________________")
+print("next question is -")
+print("What is capital of usa ?")
+z=int(input(" 1 for new york \n 2 for paris \n 3 for st.fransisco "))
+if z==1 :
+  print("go and kick yourself")
+  print("______________________________________________________________")
+elif z==2 :
+  print("bump your head")
+  print("___________________________________________________________________")
+else :
+  print("you can do nothing")
+  print("___________________________________________________________________")
+print("which country spread corona?")
+y=int(input(" 1 for India \n 2 for Usa \n 3 for Italy "))
+if y==1 :
+ print("fool")
+elif y==2 : 
+    print("owl")
+else :
+   print("lunatic")
+   print(" PLEASE GIVE ME A STAR")
+   print("this quiz is only for fun so dont take it serious")
+   
